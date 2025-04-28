85aca01f6b34f1dd4e6bb7d054982ac23a338563
daddylongloop <77903129+daddylongloop@users.noreply.github.com>
2025-04-28 11:14:10 -0700

    added table for tags which i forgot

--- _tables.py
+++ _tables.py
@@ -24,3 +24,32 @@ CREATE TABLE `RAW_RECIPES`.`IngredientsASGN` (`ID` INT NOT NULL AUTO_INCREMENT ,
 `recipeID` INT NOT NULL , 
 `ingredientID` INT NOT NULL , 
 PRIMARY KEY (`ID`)) ENGINE = InnoDB;"""
+
+CREATE_TABLE_TAGS = """
+CREATE TABLE `tags` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB
+
+INSERT INTO `tags` (`ID`, `name`) VALUES
+(1, 'vegan'),
+(2, 'vegetarian'),
+(3, 'gluten-free'),
+(4, 'dairy-free'),
+(5, 'nut-free'),
+(6, 'egg-free'),
+(7, 'low-carb'),
+(8, 'low-fat'),
+(9, 'high-protein'),
+(10, 'sugar-free'),
+(11, 'side-dishes'),
+(12, 'main-dishes'),
+(13, 'desserts');
+
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`ID`);
+
+ALTER TABLE `tags`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+"""
